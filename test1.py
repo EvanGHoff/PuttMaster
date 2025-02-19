23e4b4b60b095be6c036d5efcee942a077c318ed
Zihan-Zeng <zeng242@purdue.edu>
2025-02-19 16:45:29 -0500

    image processing test 2

--- test1.py
+++ test1.py
@@ -20,12 +20,10 @@ args = vars(ap.parse_args())
 # define the lower and upper boundaries for the ball and hole
 ball_lower = (20, 150, 120)  # Yellow
 ball_upper = (40, 235, 255)  # Yellow
-#ball_lower = (0, 180, 150) # Orange
-#ball_upper = (20, 225, 255) # Orange
-#hole_lower = (150, 0, 0)      # Black (hole, to be adjusted once the actual hole is constructed) 
-#hole_upper = (180, 35, 10) # Black
-hole_lower = (0, 180, 150) # Orange
-hole_upper = (20, 225, 255) # Orange
+#ball_lower = (10, 100, 100) # Orange
+#ball_upper = (25, 255, 255) # Orange
+hole_lower = (0, 0, 0)      # Black (hole, to be adjusted once the actual hole is constructed) 
+hole_upper = (180, 255, 30) # Black
 
 pts = deque(maxlen=args["buffer"])
 positions = []  # Store (x, y) positions
@@ -41,7 +39,7 @@ time.sleep(1.0)  # Camera warm-up
 ball_detected = False
 hole_detected = False
 ball_moved = False
-'''
+
 while True:
     frame = vs.read()
     frame = frame[1] if args.get("video", False) else frame
@@ -83,16 +81,7 @@ while True:
             optimal_trajectory = (ball_center, hole_center)
             positions.append(ball_center)
             break
-    
-    cv2.imshow("Frame", frame)
-    cv2.waitKey(0)
 
-if not args.get("video", False):
-    vs.stop()
-else:
-    vs.release()
-cv2.destroyAllWindows()
-'''
 while True:
     frame = vs.read()
     frame = frame[1] if args.get("video", False) else frame
@@ -102,44 +91,15 @@ while True:
     frame = imutils.resize(frame, width=600)
     blurred = cv2.GaussianBlur(frame, (11, 11), 0)
     hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
-
-    # Ball detection
     ball_mask = cv2.inRange(hsv, ball_lower, ball_upper)
     ball_mask = cv2.erode(ball_mask, None, iterations=2)
     ball_mask = cv2.dilate(ball_mask, None, iterations=2)
     ball_cnts = cv2.findContours(ball_mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     ball_cnts = imutils.grab_contours(ball_cnts)
 
-    # Hole detection
-    hole_mask = cv2.inRange(hsv, hole_lower, hole_upper)
-    hole_mask = cv2.erode(hole_mask, None, iterations=2)
-    hole_mask = cv2.dilate(hole_mask, None, iterations=2)
-    hole_cnts = cv2.findContours(hole_mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    hole_cnts = imutils.grab_contours(hole_cnts)
-
-    if ball_cnts and not ball_detected:
-        print("Ball Detected")
-        ball_detected = True
-        c = max(ball_cnts, key=cv2.contourArea)
-        ((ball_x, ball_y), ball_radius) = cv2.minEnclosingCircle(c)
-        ball_center = (int(ball_x), int(ball_y))
-    if hole_cnts and not hole_detected:
-        print("Hole Detected")
-        hole_detected = True
-        h = max(hole_cnts, key=cv2.contourArea)
-        ((hole_x, hole_y), hole_radius) = cv2.minEnclosingCircle(h)
-        hole_center = (int(hole_x), int(hole_y))
-        if ball_detected:
-            cv2.line(frame, ball_center, hole_center, (0, 255, 0), 2)
-            optimal_trajectory = (ball_center, hole_center)
-            positions.append(ball_center)
-
-    
-    
     center = None
-    if len(ball_cnts) > 0 and len(hole_cnts) > 0 and ball_detected and hole_detected: 
-    #if len(ball_cnts) > 0:
-        cv2.circle(frame, hole_center, int(hole_radius), (255, 0, 0), -1)
+    #if len(ball_cnts) > 0 and len(hole_cnts) > 0 and ball_detected and hole_detected: 
+    if len(ball_cnts) > 0:
         c = max(ball_cnts, key=cv2.contourArea)
         ((x, y), radius) = cv2.minEnclosingCircle(c)
         M = cv2.moments(c)
